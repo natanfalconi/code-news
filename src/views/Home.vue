286a1da28428eb541b0f992bd1a9d2b0604ee155
natan.gtecnologia <natanfalconi.gtecnologia@gmail.com>
2023-07-12 14:28:47 -0300

    test last posts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,6 @@ export default defineComponent({
         const router = useRouter()
 
         const listArticle = ref([])
-        
 
         const getListArticle = async () => {
             try {
@@ -40,28 +39,49 @@ export default defineComponent({
 </script>
 
 <template>
-    <div class="sm:w-1/2 w-10/12 mx-auto my-10 min-h-screen">
-        <article class="my-10" v-for="(article, index) in listArticle" :key="index">
-            <div class="hover:shadow-sm py-8 px-4 hover:bg-slate-700 rounded-md transition duration-150 ease-out hover:ease-in">
+    <div class="sm:w-4/5 mx-auto flex gap-4">
+        <div class="sm:w-2/3 w-10/12 my-10 min-h-screen">
+            <article class="my-10" v-for="(article, index) in listArticle" :key="index">
+                <div
+                    class="hover:shadow-sm py-8 px-4 hover:bg-slate-700 rounded-md transition duration-150 ease-out hover:ease-in">
 
-                <h2 class="text-4xl cursor-pointer font-semibold mb-5 font-mono text-indigo-500 hover:text-blue-400 transition duration-150" @click="handleClick(article)">{{ article.title }}</h2>
-                <p class="mb-5 text-gray-100 font-thin" v-html="article.description.substr(0, 200) + ' ...'"></p>
+                    <h2 class="text-4xl cursor-pointer font-semibold mb-5 font-mono text-indigo-500 hover:text-blue-400 transition duration-150"
+                        @click="handleClick(article)">{{ article.title }}</h2>
+                    <p class="mb-5 text-gray-100 font-thin" v-html="article.description.substr(0, 200) + ' ...'"></p>
 
-                <div class="flex sm:justify-end gap-3 mb-4 justify-start">
-                    <a href="https://linkedin.com.br/in/natan-falconi"
-                        class="transition duration-150 border-b-4 border-transparent hover:border-blue-500" target="_blank">
-                        <p class="text-blue-400 font-mono p-2 rounded-md">{{ article?.author?.name }} | {{ new
-                            Date(article?.created_at).toLocaleDateString() }}</p>
-                    </a>
+                    <div class="flex sm:justify-end gap-3 mb-4 justify-start">
+                        <a href="https://linkedin.com.br/in/natan-falconi"
+                            class="transition duration-150 border-b-4 border-transparent hover:border-blue-500"
+                            target="_blank">
+                            <p class="text-blue-400 font-mono p-2 rounded-md">{{ article?.author?.name }} | {{ new
+                                Date(article?.created_at).toLocaleDateString() }}</p>
+                        </a>
+                    </div>
+
+                    <button
+                        class="block bg-red-600 hover:opacity-90 rounded-full w-32 h-8 text-gray-100 font-mono font-bold uppercase"
+                        @click="handleClick(article)">
+                        Leia mais >
+                    </button>
                 </div>
 
-                <button
-                    class="block bg-red-600 hover:opacity-90 rounded-full w-32 h-8 text-gray-100 font-mono font-bold uppercase"
-                    @click="handleClick(article)">
-                    Leia mais >
-                </button>
-            </div>
+            </article>
+        </div>
 
-        </article>
+        <div class="sm:w-1/3 min-h-screen my-10">
+            <div class="my-10 shadow-sm bg-slate-700 rounded-md py-8 px-4">
+                <h2
+                    class="text-4xl font-semibold mb-5 font-mono text-indigo-500 hover:text-blue-400 transition duration-150">
+                    Ultimos Posts
+                </h2>
+
+                <ul v-for="(article, index) in listArticle" :key="index">
+                    <li
+                        class="cursor-pointer text-lg text-gray-200 transition duration-150 border-b-4 border-transparent hover:border-blue-500" @click="handleClick(article)">
+                        {{ index + 1 }} - {{ article.title }}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
