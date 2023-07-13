28000e576242277d38912c4a022a5395705f3805
natan.gtecnologia <natanfalconi.gtecnologia@gmail.com>
2023-07-13 08:20:40 -0300

    change security site

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = import.meta.env.SUPABASE_URL
-// const supabaseKey = import.meta.env.SUPABASE_KEY
-const supabaseUrl = 'https://mrxlbblrttjnkdxxsciz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yeGxiYmxydHRqbmtkeHhzY2l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg4MzI1OTksImV4cCI6MjAwNDQwODU5OX0.Xu2hqRTSDwUHbdtpr5CV7_81HUT4XRTQ0WDrJkldVwo'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default function useSupabase() {
