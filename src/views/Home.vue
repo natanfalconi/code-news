9af1cc1747f2a4997753094b7b98f442cd3de06c
natan.gtecnologia <natanfalconi.gtecnologia@gmail.com>
2023-07-14 14:48:38 -0300

    changes home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,20 +41,21 @@ export default defineComponent({
 <template>
     <div class="sm:w-4/5 mx-auto flex flex-col-reverse sm:flex-row sm:flex gap-4">
         <div class="sm:w-2/3 w-10/12 mx-auto sm:my-10 min-h-screen">
-            <article class="mb-10" v-for="(article, index) in listArticle" :key="index">
+            <article class="mb-10 hover:shadow-lg" v-for="(article, index) in listArticle" :key="index">
                 <div
                     class="shadow-sm py-8 px-4 border border-slate-700 hover:bg-slate-700 rounded-md transition duration-150 ease-out hover:ease-in">
 
-                    <h2 class="sm:text-4xl text-3xl cursor-pointer font-semibold mb-5 font-mono text-indigo-500 hover:text-blue-400 transition duration-150"
+                    <h2 class="sm:text-4xl text-3xl cursor-pointer font-semibold mb-5 font-mono text-indigo-500 hover:underline"
                         @click="handleClick(article)">{{ article.title }}</h2>
                     <p class="mb-5 text-gray-100 font-thin" v-html="article.description.substr(0, 210) + ' ...'"></p>
 
                     <div class="flex sm:justify-end gap-3 mb-4 justify-start">
-                        <a href="https://linkedin.com.br/in/natan-falconi"
+                        <a :href="article?.author?.social_link"
                             class="transition duration-150 border-b-4 border-transparent hover:border-blue-500"
                             target="_blank">
-                            <p class="text-blue-400 font-mono py-2 rounded-md sm:text-md text-sm">{{ article?.author?.name }} | {{ new
-                                Date(article?.created_at).toLocaleDateString() }}</p>
+                            <p class="text-blue-400 font-mono rounded-md sm:text-md text-sm">{{ article?.author?.name }} |
+                                {{ new
+                                    Date(article?.created_at).toLocaleDateString() }}</p>
                         </a>
                     </div>
 
@@ -75,9 +76,10 @@ export default defineComponent({
                 </h2>
 
                 <ul v-for="(article, index) in listArticle" :key="index">
-                    <li class="cursor-pointer text-lg text-gray-200 "
+                    <li class="cursor-pointer text-lg text-gray-200 flex gap-1"
                         @click="handleClick(article)">
-                        <p class="inline-block transition duration-150 border-b-4 border-transparent hover:border-blue-500">{{ index + 1 }} - {{ article.title }}</p>
+                        <p>{{ index + 1 }}.</p>
+                        <p class="inline-block transition duration-150 hover:underline hover:text-blue-500">{{ article.title }}</p>
                     </li>
                 </ul>
             </div>
